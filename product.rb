eccfec95e94d06eab40c255f779d16712a29b61b
codonculous <feidameng01@gmail.com>
2017-04-02 14:31:59 -0400

    define init method for both class and define add_product method for shopping_cart class

--- product.rb
+++ product.rb
@@ -1,4 +1,9 @@
 class Product
 
+  def initialize(name,base_price,tax_rate)
+    @name = name
+    @base_price = base_price
+    @tax_rate = tax_rate
+  end
 
 end
--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,4 +1,32 @@
+require_relative "product"
+
 class ShoppingCart
 
 
+  def initialize(argument)
+    @argument = argument
+    @products = []
+  end
+
+  def add_products(name,base_price,tax_rate)
+    @products.push(Product.new(name,base_price,tax_rate))
+
+  end
+
 end
+
+
+# Each shopping cart has a collection of products. It should also have the following functionality:
+#
+# add an product to the cart
+# remove an product from the cart
+# add up the total cost of all products in the cart before tax
+# add up the total cost of all products in the cart after tax
+
+# Add the ability to find the most expensive product in a cart.
+# Allow a quantity to be associated with each product in the cart.
+# What is the best way to store this information?
+# How does it affect each of your other methods?
+# Instead of storing the tax rate for each product,
+# come up with a tax classification system
+# (eg. standard, tax exempt, imported) so the rates are standardized across all products.
